8940d53f036bc9b0ec67f132859101d35b2a49e2
Gustavo <Gustavo>
2021-03-12 17:29:58 -0300

    Finalizando Gritador

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+           Console.Beep();
+
+           string textoPadrao;
+
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           Console.Write("Palmeiras tem mundial? ");
+           
+           Console.ResetColor();
+           textoPadrao = Console.ReadLine();
+
+           
+
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           Console.Write("Sua resposta: ");
+
+           Console.ForegroundColor = ConsoleColor.DarkRed;
+           Console.Write($"{textoPadrao.ToUpper()}!");
+           Console.ResetColor();
+
+           Console.Beep();
+        }  
+    }
+}
